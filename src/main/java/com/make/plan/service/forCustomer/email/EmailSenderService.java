0033a876969fa8980ba0f9b740139ced71002cb2
GustavEiffels <sibuk9315@gmail.com>
2022-04-25 18:35:44 +0900

    last

--- src/main/java/com/make/plan/service/forCustomer/email/EmailSenderService.java
+++ src/main/java/com/make/plan/service/forCustomer/email/EmailSenderService.java
@@ -11,9 +11,9 @@ import java.util.Properties;
 @Service
 public class EmailSenderService {
     public void sendMail(String title, String sendTo, String content) throws MessagingException {
-        final String user = "";
-        final String password = "";
-        final String sender ="";
+        final String user = "|";
+        final String password = "|";
+        final String sender ="|";
         String smtpMailLocate = "smtp.naver.com";
 
         Properties prop = new Properties();
